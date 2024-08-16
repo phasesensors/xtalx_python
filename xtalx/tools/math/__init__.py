ece95fd4df526f38330bd6bd7c3f21cbe5ede294
Terry Greeniaus <tgreeniaus@phasesensors.com>
2024-08-15 19:10:01 -0600

    tools/math: Add hex functions to deal with double-precision floating-point data.
    These functions help convert binary representations of a
    double-precision number into their Python numerical equivalents.

--- xtalx/tools/math/__init__.py
+++ xtalx/tools/math/__init__.py
@@ -1,5 +1,6 @@
 # Copyright (c) 2022-2024 by Phase Advanced Sensor Systems, Inc.
 # All rights reserved.
+from .hex_funcs import hex_to_double, hex_s_to_double
 from .lorentz import Lorentzian
 from .xy_series import XYSeries
 from .polynomial_fit import PolynomialFit1D, PolynomialFit2D
@@ -9,4 +10,6 @@ __all__ = ['Lorentzian',
            'PolynomialFit1D',
            'PolynomialFit2D',
            'XYSeries',
+           'hex_s_to_double',
+           'hex_to_double',
            ]
