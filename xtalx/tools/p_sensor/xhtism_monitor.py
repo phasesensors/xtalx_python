eea3476e3ae0eb5dd500a64dbbfc1de68c69eae1
Terry Greeniaus <tgreeniaus@phasesensors.com>
2025-05-21 17:43:23 -0600

    p_sensor/xhtism_monitor.py: Allow talking to multiple sensors on the same bus.

--- xtalx/tools/p_sensor/xhtism_monitor.py
+++ xtalx/tools/p_sensor/xhtism_monitor.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # Copyright (c) 2024 by Phase Advanced Sensor Systems, Inc.
 # All rights reserved.
+import threading
 import argparse
 import logging
 import time
@@ -16,31 +17,20 @@ import xtalx.tools.modbus.serial
 LOG_LEVEL = logging.INFO
 
 
-def make_sensor(rv):
-    if rv.intf:
-        bus = xtalx.tools.modbus.serial.Bus(rv.intf, rv.baud_rate)
-        return xtalx.p_sensor.XHTISM(bus, int(rv.addr, 0))
+def sensor_thread(xhtism, idb):
+    # Monitor the sensor.
+    logging.info('%s: Monitoring...', xhtism.serial_num)
+    for m in xhtism.yield_measurements():
+        if idb:
+            point = m.to_influx_point()
+            if point['fields']:
+                idb.append(point)
 
-    dev = xtalx.modbus_adapter.find_one_mba(
-            serial_number=rv.modbus_adapter_serial_number)
-    if dev is not None:
-        bus = xtalx.modbus_adapter.make_mba(dev, baud_rate=rv.baud_rate)
-        bus.set_vext(True)
-        time.sleep(0.1)
-        return xtalx.p_sensor.XHTISM(bus, int(rv.addr, 0))
-
-    raise Exception('No matching devices.')
+        logging.info('%s: tf %.6f pf %.6f ',
+                     xhtism.serial_num, m.temp_freq, m.pressure_freq)
 
 
 def main(rv):
-    # Make the sensor.
-    xhtism = make_sensor(rv)
-    logging.info('%s: Found sensor with firmware version %s, git SHA1 %s',
-                 xhtism.serial_num, xhtism.fw_version_str, xhtism.git_sha1)
-    t_c, p_c = xhtism.get_coefficients()
-    logging.info('%s: T Coefficient: %u', xhtism.serial_num, t_c)
-    logging.info('%s: P Coefficient: %u', xhtism.serial_num, p_c)
-
     # Read the configuration file.
     if rv.config:
         logging.info('%s: Reading configuration...', xhtism.serial_num)
@@ -57,16 +47,49 @@ def main(rv):
     else:
         idb = None
 
-    # Monitor the sensor.
-    logging.info('%s: Monitoring...', xhtism.serial_num)
-    for m in xhtism.yield_measurements():
-        if idb:
-            point = m.to_influx_point()
-            if point['fields']:
-                idb.append(point)
+    # Make the bus.
+    if rv.intf:
+        bus = xtalx.tools.modbus.serial.Bus(rv.intf, rv.baud_rate)
+    else:
+        dev = xtalx.modbus_adapter.find_one_mba(
+                serial_number=rv.modbus_adapter_serial_number)
+        if dev is None:
+            raise Exception('No adapter found.')
+        bus = xtalx.modbus_adapter.make_mba(dev, baud_rate=rv.baud_rate)
+        bus.set_vext(True)
+        time.sleep(0.1)
 
-        logging.info('%s: tf %.6f pf %.6f ',
-                     xhtism.serial_num, m.temp_freq, m.pressure_freq)
+    # Spawn a thread for each sensor on the bus.
+    xhtisms = []
+    threads = []
+    addrs = rv.addr
+    if addrs is None:
+        addrs = ['0x80']
+    for addr in addrs:
+        xhtism = xtalx.p_sensor.XHTISM(bus, int(addr, 0))
+        logging.info('%s: Found sensor with firmware version %s, git SHA1 %s',
+                     xhtism.serial_num, xhtism.fw_version_str, xhtism.git_sha1)
+        t_c, p_c = xhtism.get_coefficients()
+        logging.info('%s: T Coefficient: %u', xhtism.serial_num, t_c)
+        logging.info('%s: P Coefficient: %u', xhtism.serial_num, p_c)
+        xhtisms.append(xhtism)
+
+        t = threading.Thread(target=sensor_thread, args=(xhtism, idb))
+        t.start()
+        threads.append(t)
+
+    # Run forever.
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        print()
+
+    # Halt and join all threads for a clean exit.
+    for xhtism in xhtisms:
+        xhtism.halt_yield()
+    for t in threads:
+        t.join()
 
 
 def _main():
@@ -78,7 +101,7 @@ def _main():
     parser.add_argument('--config', '-c')
     parser.add_argument('--intf', '-i')
     parser.add_argument('--baud-rate', '-b', default=115200, type=int)
-    parser.add_argument('--addr', '-a', default='0x80')
+    parser.add_argument('--addr', '-a', action='append', default=None)
     parser.add_argument('--modbus-adapter-serial-number', '-s')
 
     try:
